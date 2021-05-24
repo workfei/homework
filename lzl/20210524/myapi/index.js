4444eeb83badc9cd80667d4ae1021a113191ec00
pamper-o <lzl041220@163.com>
2021-05-24 22:30:07 +0800

    刘志林今日作业

---
+++ lzl/20210524/myapi/index.js
@@ -0,0 +1,160 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+// 处理静态资源
+app.use(express.static('public'))
+// 处理参数
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// 设置允许跨域访问该服务
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'mytoken');
+  next();
+});
+app.get('/async1', (req, res) => {
+  res.send('hello1')
+})
+app.get('/async2', (req, res) => {
+  if (req.query.info == 'hello') {
+    res.send('world')
+  } else {
+    res.send('error')
+  }
+})
+
+app.get('/adata', (req, res) => {
+  res.send('Hello axios!')
+})
+app.get('/axios', (req, res) => {
+  res.send('axios get 传递参数' + req.query.id)
+})
+app.get('/axios/:id', (req, res) => {
+  res.send('axios get (Restful) 传递参数' + req.params.id)
+})
+app.delete('/axios', (req, res) => {
+  res.send('axios get 传递参数' + req.query.id)
+})
+app.post('/axios', (req, res) => {
+  res.send('axios post 传递参数' + req.body.uname + '---' + req.body.pwd)
+})
+app.put('/axios/:id', (req, res) => {
+  res.send('axios put 传递参数' + req.params.id + '---' + req.body.uname + '---' + req.body.pwd)
+})
+
+app.get('/axios-json', (req, res) => {
+  res.json({
+    uname: 'lisi',
+    age: 12
+  });
+})
+
+
+app.get('/fdata', (req, res) => {
+  res.send('Hello Fetch!')
+})
+app.get('/books', (req, res) => {
+  res.send('传统的URL传递参数!' + req.query.id)
+})
+app.get('/books/:id', (req, res) => {
+  res.send('Restful形式的URL传递参数!' + req.params.id)
+})
+app.delete('/books/:id', (req, res) => {
+  res.send('DELETE请求传递参数!' + req.params.id)
+})
+app.post('/books', (req, res) => {
+  res.send('POST请求传递参数!' + req.body.uname + '---' + req.body.pwd)
+})
+app.put('/books/:id', (req, res) => {
+  res.send('PUT请求传递参数!' + req.params.id + '---' + req.body.uname + '---' + req.body.pwd)
+})
+
+app.get('/json', (req, res) => {
+  res.json({
+    uname: 'lisi',
+    age: 13,
+    gender: 'male'
+  });
+})
+
+app.get('/a1', (req, res) => {
+  setTimeout(function () {
+    res.send('Hello TOM!')
+  }, 1000);
+})
+app.get('/a2', (req, res) => {
+  setTimeout(function () {
+    res.send('Hello JERRY!')
+  }, 2000);
+})
+app.get('/a3', (req, res) => {
+  setTimeout(function () {
+    res.send('Hello SPIKE!')
+  }, 3000);
+})
+
+// 路由
+app.get('/data', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/data1', (req, res) => {
+  setTimeout(function () {
+    res.send('Hello TOM!')
+  }, 1000);
+})
+app.get('/data2', (req, res) => {
+  res.send('Hello JERRY!')
+})
+
+
+const databox = [{
+  id: 1,
+  name: '星明',
+  age: 20,
+  pwd: '123456',
+  like: '琴棋书画'
+}, {
+  id: 2,
+  name: '刘志林',
+  age: 18,
+  pwd: '123456',
+  like: '琴棋书画样样不行'
+}, {
+  id: 3,
+  name: '黄奇特',
+  age: 22,
+  pwd: '123546',
+  like: '琴棋书画略懂一点'
+}, {
+  id: 4,
+  name: '李永恒',
+  age: 20,
+  pwd: '123456',
+  like: '琴棋书画一窍不通'
+}]
+app.get('/databox', (req, res) => {
+  res.send(databox);
+})
+
+
+//用户登录
+app.get('/login', (req,res) => {
+  var name = req.query.name;
+  var pwd = req.query.pwd;
+  if (name == '李永恒' && pwd == '123456') {
+    res.send('ok');
+  } else {
+    res.send('err');
+  }
+})
+
+// 启动监听
+app.listen(3003, () => {
+  console.log('running...')
+})
