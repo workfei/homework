dec46975da425919ff167ee3ed5f4c528106f5e1
mn42 <2412274790@qq.com>
2021-05-30 20:42:19 +0800

    05-30+js作业

---
+++ dxm/20210530/src/components/userlist.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-table :data="userlist" border style="width: 100%">
+    <el-table-column prop="id" label="学号" width="180"> </el-table-column>
+    <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+    <el-table-column prop="age" label="年龄" width="180"> </el-table-column>
+    <el-table-column prop="like" label="爱好"> </el-table-column>
+  </el-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+          userlist: []
+      }
+    },
+    methods: {
+        getUserlist () {
+            this.$http.get('/getUsers').then(res => {
+                this.userlist = res
+            })
+        }
+    },
+    mounted () {
+        this.getUserlist()
+    }
+  }
+</script>
